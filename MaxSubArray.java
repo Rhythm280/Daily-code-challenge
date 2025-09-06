05636f4f7bda944da7d0dba8daaff7719be49afc
Rhythm@28 <rhythm2805@gmail.com>
2025-09-06 23:21:03 +0530

    Day 2 | solved

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -11,16 +11,13 @@ public class MaxSubArray {
         
         for (int i = 0; i < nums.length; i++) {
             currentSum += nums[i];
-            
             if (currentSum > maxSum) {
                 maxSum = currentSum;
             }
-            
             if (currentSum < 0) {
                 currentSum = 0;
             }
         }
-        
         return maxSum;
     }
 }
