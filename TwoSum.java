5f52288d08d5d6ab656f06f152e67168e481f0a3
Rhythm@28 <rhythm2805@gmail.com>
2025-09-07 00:58:16 +0530

    Day-1 | Arrays | Two Sum | Solved

--- TwoSum.java
+++ TwoSum.java
@@ -7,7 +7,7 @@ public class TwoSum {
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
         int[] result = solution.twoSum(nums, target);
-        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        System.out.println(result.toString());
     }
 
     public int[] twoSum(int[] nums, int target) {
