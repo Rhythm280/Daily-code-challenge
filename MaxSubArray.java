38f80803a8ea971f5d99f2d386f5c010fb2f0c47
Rhythm@28 <rhythm2805@gmail.com>
2025-09-06 22:11:18 +0530

    Day-2 | Arrays | Maximum Subarray (Kadane's Algorithm) | Solved

---
+++ MaxSubArray.java
@@ -0,0 +1,26 @@
+public class MaxSubArray {
+    public static void main(String[] args) {
+        MaxSubArray solution = new MaxSubArray();
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int result = solution.maxSubArray(nums);
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
