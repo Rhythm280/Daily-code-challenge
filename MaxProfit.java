773b91bc6634df42db32c25ae8b5a7e5ba075958
Rhythm@28 <rhythm2805@gmail.com>
2025-09-12 12:05:08 +0530

    Day 4 | Arrays | Best Time to Buy and Sell Stock | Solved

---
+++ MaxProfit.java
@@ -0,0 +1,21 @@
+public class MaxProfit {
+    public static void main(String[] args) {
+        MaxProfit solution = new MaxProfit();
+        int[] prices = {7,1,5,3,6,4};
+        int result = solution.maxProfit(prices);
+        System.out.println(result);
+    }
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.max(profit, maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+}
--- TwoSum.java
+++ TwoSum.java
@@ -7,7 +7,7 @@ public class TwoSum {
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
         int[] result = solution.twoSum(nums, target);
-        System.out.println(result.toString());
+        System.out.println(result[0] + " " + result[1]);
     }
 
     public int[] twoSum(int[] nums, int target) {
